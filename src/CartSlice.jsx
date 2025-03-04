04d2848f50686982b4e9199481ba205e21d53ab8
Luciano <lucianoraspoo@users.noreply.github.com>
2025-03-04 16:25:27 -0300

    Finish Final Poject e-plantShopping

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -17,10 +17,9 @@ export const CartSlice = createSlice({
       }
     },
     removeItem: (state, action) => {
-
       state.items = state.items.filter(item => item.name !== action.payload);
-
     },
+    
     updateQuantity: (state, action) => {
       const { name, quantity } = action.payload;
       const itemToUpdate = state.items.find(item => item.name === name);
